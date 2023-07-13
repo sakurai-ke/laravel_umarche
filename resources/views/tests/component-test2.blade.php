d1ee3749ada9b81ccd023a106a3b89c93491bcbe
sakurai-ke <sakurai-ke@c00.itscom.net>
2023-07-13 09:03:56 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -2,8 +2,13 @@
     <x-slot name="header">ヘッダー2</x-slot>
   
     コンポーネントテスト2
-    {{-- <x-test-class-base classBaseMessage="メッセージです" />
+{{-- Http>View>Components>TestClassBase.phpを呼び、その中に指定された（public function render()....で指定された）
+    test-class-base-component.phpの記述内容が表示される --}}
+    {{-- <x-test-class-base /> --}}
+
+{{-- 属性（classBaseMessage="メッセージです）について、Http>View>Components>TestClassBase.phpのクラスにも設定する必要がある --}}
+    <x-test-class-base classBaseMessage="メッセージです" />
     <div class="mb-4"></div>
-    <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更しています" /> --}}
+    <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更しています" />
   
   </x-tests.app>
